8a353462345f0f473be8d68d7a4732fca8bfbe90
benzrf <benzrf@benzrf.com>
2014-06-09 19:28:29 -0400

    Switched to proper option parsing.

--- Main.hs
+++ Main.hs
@@ -1,16 +1,35 @@
 module Main where
 
 import Debug.Vampire
-import System.Environment
-import Data.Functor
+import Options.Applicative
+import Control.Applicative
+import Control.Monad
+import System.Exit
 
-main = do
-  args <- getArgs
-  case args of
-    fn:_ -> do
-      newCode <- rewriteFile <$> readFile fn
-      case newCode of
-        Just code -> putStrLn code
-        Nothing -> putStrLn "Invalid syntax in file!"
-    [] -> putStrLn "Need a filename!"
+readFrom "-"  = getContents
+readFrom file = readFile file
+writeTo  "-"  = putStrLn
+writeTo  file = writeFile file
+
+doRewrite :: String -> String -> IO ()
+doRewrite src dest = do
+  code <- readFrom src
+  case rewriteFile code of
+    Just newCode -> writeTo dest newCode
+    Nothing      -> putStrLn "could not parse input" >> exitFailure
+
+-- parsers!
+
+rewrite =
+  liftA2 doRewrite
+    (argument str $ metavar "SOURCE")
+    (argument str $ metavar "DEST" <> value "-")
+rewriteDesc = "Rewrite a Haskell source file to add Vampire tracing."
+ 
+vampire = subparser $
+  (command "rewrite" $ info rewrite $ progDesc rewriteDesc)
+
+main = join $ execParser $
+  info (helper <*> vampire)
+  (fullDesc <> header "vampire - eeevil expression tree analysis")
 
